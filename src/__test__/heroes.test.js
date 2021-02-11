4281055672740992aea211b79011754e559f580e
theart84 <artshadow74@gmail.com>
2021-02-11 18:20:16 +0500

    Task completed

---
+++ src/__test__/heroes.test.js
@@ -0,0 +1,15 @@
+import sortHeroes from '../js/heroes';
+
+test('Тестируем функцию на возврат корректного значения', () => {
+  const received = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const data = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  expect(sortHeroes(data)).toEqual(received);
+});
